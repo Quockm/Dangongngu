48cd3ea5715746eea17f787e5d9b795ac8019306
Quockm <mjnhquoc21101997@gmail.com>
2020-08-28 19:04:35 +0700

    setLanguageLocal

--- app/src/main/java/com/example/dangonngu13072020/MainActivity.java
+++ app/src/main/java/com/example/dangonngu13072020/MainActivity.java
@@ -2,26 +2,34 @@ package com.example.dangonngu13072020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText mEdtAccount,mEditPass;
+    EditText mEdtAccount, mEditPass;
     TextView mtxtInfo;
-    Button mBtnLogin;
+    Button mBtnLogin, mBtnEn, mBtnVi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setLanguageLocal("vi");
         setContentView(R.layout.activity_main);
 
         mEdtAccount = findViewById(R.id.editTextAccount);
         mEditPass = findViewById(R.id.editTextPassWord);
         mBtnLogin = findViewById(R.id.buttonLogin);
         mtxtInfo = findViewById(R.id.textViewInFo);
+        mBtnEn = findViewById(R.id.btnEn);
+        mBtnVi = findViewById(R.id.btnVi);
+
 
         mBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,9 +41,17 @@ public class MainActivity extends AppCompatActivity {
                 String titePassword = getResources().getString(R.string.MATKHAU);
 
                 mtxtInfo.setText(String.format("%s : %s\n%s : %s",
-                        titeAccount,textAccount,titePassword,textPass));
+                        titeAccount, textAccount, titePassword, textPass));
             }
         });
+    }
+
+    private void setLanguageLocal(String Key) {
+        Locale locale = new Locale(Key);
+        Locale.setDefault(locale);
+        Configuration configuration = new Configuration(getResources().getConfiguration());
+        configuration.setLocale(locale);
+        getResources().updateConfiguration(configuration,getResources().getDisplayMetrics());
     }
 
 }
